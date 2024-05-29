e3c342ebbad7fe9ac5a550bde92bf41c68d8003d
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-05-29 14:50:26 +0200

    refactor contao-autocompletejs-bundle.js for better clarity

--- src/Resources/assets/js/contao-autocompletejs-bundle.js
+++ src/Resources/assets/js/contao-autocompletejs-bundle.js
@@ -1,116 +1,126 @@
 import autoComplete from "@tarekraafat/autocomplete.js/src/autoComplete.js";
 
 class AutocompletejsBundle {
+
     static init() {
-        const turnOffAutocomplete = (node) => {
-            node.setAttribute('autocomplete', 'off');
+        setTimeout(() => AutocompletejsBundle.initAutocompleteJs(), 100);
+    }
+
+    static initAutocompleteJs() {
+        let autocompleteFields = document.querySelectorAll("input[data-autocompletejs='1']");
+
+        if (autocompleteFields.length === 0) {
+            return;
+        }
+
+        autocompleteFields.forEach((field) => {
+            AutocompletejsBundle.initField(field);
+        });
+    }
+
+    static elementCallback(item, data) {
+        if (typeof data.match === 'object') {
+            item.innerHTML = Object.values(data.match)[0];
+        } else {
+            item.innerHTML = data.match;
+        }
+        document.dispatchEvent(
+            new CustomEvent('huh.autocompletejs.adjust_result_item', {
+                bubbles: true,
+                cancelable: true,
+                detail: {
+                    source: item,
+                    data: data
+                }
+            })
+        );
+    }
+
+    static initField(field) {
+        if (field.autoCompleteInitialied === true) {
+            return;
+        }
+        field.autoCompleteInitialied = true;
+
+        let ctx = JSON.parse(field.dataset.autocompletejsOptions);
+
+        if (!ctx.selector) {
+            if (typeof ctx.selector === 'undefined' && typeof field.id !== 'undefined') {
+                ctx.selector = '#' + field.id;
+            } else {
+                ctx.selector = () => field;
+            }
+        }
+
+        field.setAttribute('autocomplete', 'off');
+
+        AutocompletejsBundle.configureData(ctx);
+
+        // decrease threshold since
+        ctx.threshold = ctx.threshold - 1;
+
+        let maxResults = (ctx.resultsList && ctx.resultsList.maxResults) ? ctx.resultsList.maxResults : ctx.maxResults ?? 5;
+        delete ctx.maxResults;
+
+        // configuration of the resultsList to prevent PAIN
+        ctx.resultsList = {
+            tabSelection: true,
+            id: 'autocomplete_' + field.id,
+            class: 'autocomplete_results_container',
+            position: 'afterend',
+            maxResults: maxResults,
         };
 
-        setTimeout(() => {
-            let autocompleteFields = document.querySelectorAll("input[data-autocompletejs='1']");
+        // configuration of resultItem
+        // with CustomEvent to modify results
+        ctx.resultItem = {
+            tag: 'li',
+            id: 'autoComplete_result_' + field.id,
+            class: 'autoComplete_result',
+            element: AutocompletejsBundle.elementCallback,
+            highlight: ctx.highlight
+        };
 
-            if (autocompleteFields.length !== 0) {
-                autocompleteFields.forEach((field) => {
+        // remove searchEngine if set to none
+        if (ctx.searchEngine === 'none') {
+            ctx.searchEngine = (query, record) => {
+                return record;
+            };
+        }
 
-                    let ctx = JSON.parse(field.dataset.autocompletejsOptions);
+        let ac = new autoComplete(ctx);
 
-                    if (!ctx.selector) {
-                        if (typeof ctx.selector === 'undefined' && typeof field.id !== 'undefined') {
-                            ctx.selector = '#' + field.id;
-                        } else {
-                            ctx.selector = () => field;
-                        }
-                    }
+        field.addEventListener('focus', (e) => {
+            console.log('focus')
+            ac.start();
+        });
 
-                    turnOffAutocomplete(field);
+        field.addEventListener('blur', (e) => {
+            ac.close();
+        });
 
-                    AutocompletejsBundle.configureData(ctx);
+        field.addEventListener('selection', e => {
 
-                    // decrease threshold since
-                    ctx.threshold = ctx.threshold - 1;
+            let value = e.detail.selection.value;
 
-                    let maxResults = (ctx.resultsList && ctx.resultsList.maxResults) ? ctx.resultsList.maxResults : ctx.maxResults ?? 5;
-                    delete ctx.maxResults;
-
-                    // configuration of the resultsList to prevent PAIN
-                    ctx.resultsList = {
-                        tabSelection: true,
-                        id: 'autocomplete_' + field.id,
-                        class: 'autocomplete_results_container',
-                        position: 'afterend',
-                        maxResults: maxResults,
-                    };
-
-                    // configuration of resultItem
-                    // with CustomEvent to modify results
-                    ctx.resultItem = {
-                        tag: 'li',
-                        id: 'autoComplete_result_' + field.id,
-                        class: 'autoComplete_result',
-                        element: (item, data) => {
-
-                            if(typeof data.match === 'object') {
-                                item.innerHTML = Object.values(data.match)[0];
-                            } else {
-                                item.innerHTML = data.match;
-                            }
-
-                            document.dispatchEvent(
-                                new CustomEvent('huh.autocompletejs.adjust_result_item', {
-                                    bubbles: true,
-                                    cancelable: true,
-                                    detail: {
-                                        source: item,
-                                        data: data
-                                    }
-                                })
-                            );
-                        },
-                        highlight: ctx.highlight
-                    };
-
-                    // remove searchEngine if set to none
-                    if (ctx.searchEngine === 'none') {
-                        ctx.searchEngine = (query, record) => {
-                            return record;
-                        };
-                    }
-
-                    let ac = new autoComplete(ctx);
-
-                    field.addEventListener('focus', (e) => {
-                        ac.start();
-                    });
-
-                    field.addEventListener('blur', (e) => {
-                        ac.close();
-                    });
-
-                    field.addEventListener('selection', e => {
-
-                        let value = e.detail.selection.value;
-
-                        if (ctx.data.keys) {
-                            value = e.detail.selection.value[ctx.data.keys[0]];
-                        }
-
-                        // document.querySelector('#' + field.id).value = value;
-                        field.value = value;
-                        document.dispatchEvent(
-                            new CustomEvent('huh.autocompletejs.onselection', {
-                                bubbles: true,
-                                cancelable: true,
-                                detail: {
-                                    field: field,
-                                    item: e.detail
-                                }
-                            })
-                        );
-                    });
-                });
+            if (ctx.data.keys) {
+                value = e.detail.selection.value[ctx.data.keys[0]];
             }
-        }, 100);
 
+            // document.querySelector('#' + field.id).value = value;
+            field.value = value;
+
+            document.dispatchEvent(
+                new CustomEvent('huh.autocompletejs.onselection', {
+                    bubbles: true,
+                    cancelable: true,
+                    detail: {
+                        field: field,
+                        item: e.detail
+                    }
+                })
+            );
+        });
     }
 
     static configureData(options) {
